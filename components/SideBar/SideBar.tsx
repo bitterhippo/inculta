33c4e2886028e2b5376dae5dd89af18dcda5c0b8
neror <bitterhippo@gmail.com>
2026-01-29 19:11:25 -0800

    feat(sidebar): basic component structure added

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,7 +1,17 @@
-const SideBar = async () => {
+"use client";
+
+import styles from "./styles.module.css";
+import { useState } from "react";
+
+const SideBar = ({ children }: { children: React.ReactNode }) => {
+  const [collapse, setCollapsed] = useState<boolean>(false);
+
   return (
-    <nav>
-      <div></div>
+    <nav className={styles.SideBarContainer}>
+      <button onClick={() => setCollapsed((prev) => !prev)}>
+        {collapse ? "O" : "C"}
+      </button>
+      {!collapse && <div className={styles.contents}>{children}</div>}
     </nav>
   );
 };
