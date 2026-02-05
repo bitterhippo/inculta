7a37fb565c2277bac1dd73f2ce4c0a183d4c6093
neror <bitterhippo@gmail.com>
2026-02-04 23:02:04 -0800

    chore(sidebar): corrects the positioning of border and button. Includes fixes for collapsing behavior

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,17 +1,25 @@
 "use client";
 
 import styles from "./styles.module.css";
+import { ArrowLeft, ArrowRight } from "react-feather";
 import { useState } from "react";
 
 export const SideBar = ({ children }: { children: React.ReactNode }) => {
-  const [collapse, setCollapsed] = useState<boolean>(false);
+  const [collapsed, setCollapsed] = useState<boolean>(false);
 
   return (
-    <nav className={styles.SideBarContainer}>
-      <button onClick={() => setCollapsed((prev) => !prev)}>
-        {collapse ? "O" : "C"}
+    <nav
+      className={`${styles.SideBarContainer} ${
+        collapsed ? styles.collapsed : ""
+      } `}
+    >
+      <button
+        className={styles.ToggleButton}
+        onClick={() => setCollapsed((prev) => !prev)}
+      >
+        {collapsed ? <ArrowLeft size={16} /> : <ArrowRight size={16} />}
       </button>
-      {!collapse && <div className={styles.contents}>{children}</div>}
+      {!collapsed && <div className={styles.contents}>{children}</div>}
     </nav>
   );
 };
