34eb39a1eb5fcf4e910885881f3b24b46f01715b
neror <bitterhippo@gmail.com>
2026-02-08 23:00:12 -0800

    fix(DragIcon): adds conditional chaining inside the styling of componet to resolve run-time error

--- components/DragIcon/DragIcon.tsx
+++ components/DragIcon/DragIcon.tsx
@@ -8,7 +8,7 @@ export const DragIcon = ({ id, transform }: DragIconProps) => {
     <div
       className={styles.DragIcon}
       ref={setNodeRef}
-      style={{ top: transform.x, left: transform.y }}
+      style={{ top: transform?.x, left: transform?.y }}
       {...listeners}
       {...attributes}
     />
