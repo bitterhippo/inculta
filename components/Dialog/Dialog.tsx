8c61273507c6e62bf486675e434526668db74227
neror <bitterhippo@gmail.com>
2026-02-21 21:40:52 -0800

    feat(dialog): creates dialog component and associated styles

--- components/Dialog/Dialog.tsx
+++ components/Dialog/Dialog.tsx
@@ -1,5 +1,10 @@
 "use client";
 import "react";
+import styles from "./styles.module.css";
 export const Dialog = ({ onClose }) => {
-  return <div onClick={onClose}> lol </div>;
+  return (
+    <div className={styles.DialogOverlay} onClick={onClose}>
+      <div className={styles.DialogContainer}>lol</div>
+    </div>
+  );
 };
