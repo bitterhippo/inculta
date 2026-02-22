f1721a9515ad930fd854f6c50ecbfc6639b05c20
neror <bitterhippo@gmail.com>
2026-02-21 21:44:33 -0800

    chore(dialog): adds types - forgot this in the last commit

--- components/Dialog/Dialog.tsx
+++ components/Dialog/Dialog.tsx
@@ -1,7 +1,9 @@
 "use client";
 import "react";
 import styles from "./styles.module.css";
-export const Dialog = ({ onClose }) => {
+import { DialogTypes } from "./types";
+
+export const Dialog = ({ onClose }: DialogTypes) => {
   return (
     <div className={styles.DialogOverlay} onClick={onClose}>
       <div className={styles.DialogContainer}>lol</div>
---
+++ components/Dialog/types.ts
@@ -0,0 +1,3 @@
+export interface DialogTypes {
+  onClose?: React.MouseEventHandler<HTMLDivElement>;
+}
