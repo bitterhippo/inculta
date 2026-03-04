8fec02be48e537331eaca7cdfaec7f96caf35f90
neror <bitterhippo@gmail.com>
2026-03-04 13:54:05 -0800

    feat(addassetdialog): incoporates file preview now

--- domain/AddAssetDialog/AddAssetDialog.tsx
+++ domain/AddAssetDialog/AddAssetDialog.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { Dialog, FileUploadIconButton } from "@/components";
 import { AddAssetDialogTypes } from "./types";
 import styles from "./styles.module.css";
@@ -9,12 +10,27 @@ export const AddAssetDialog = ({
   setDialogOpen,
   setSelectedFile,
 }: AddAssetDialogTypes) => {
+  const [previewUrl, setPreviewUrl] = useState();
+
+  useEffect(() => {
+    if (!selectedFile) return;
+
+    const url = URL.createObjectURL(selectedFile);
+    setPreviewUrl(url);
+
+    return () => URL.revokeObjectURL(url);
+  }, [selectedFile]);
   return (
     <Dialog onClose={() => setDialogOpen(false)}>
       <FileUploadIconButton onFileSelect={setSelectedFile} />
       <span className={styles.AddAssetDialogText}>
         {selectedFile ? selectedFile?.name : "Currently no file selected."}
       </span>
+      {previewUrl && (
+        <div>
+          <img src={previewUrl} alt={selectedFile?.name} />
+        </div>
+      )}
     </Dialog>
   );
 };
