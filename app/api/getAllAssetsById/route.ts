3e8ee2b1424c340f2da15e32655e73c1230f2fd4
neror <bitterhippo@gmail.com>
2026-03-17 20:40:43 -0700

    chore(getAllAssetsById): updates route to also fetch backdrop data

--- app/api/getAllAssetsById/route.ts
+++ app/api/getAllAssetsById/route.ts
@@ -6,17 +6,24 @@ export async function POST(req: NextRequest) {
     const body = await req.json();
     const { userId } = body;
 
-    const { data, error } = await supabase
-      .from("asset")
-      .select("*")
-      .eq("userId", userId);
+    const [
+      { data: assetData, error: assetError },
+      { data: backdropData, error: backdropError },
+    ] = await Promise.all([
+      supabase.from("asset").select("*").eq("userId", userId),
+      supabase.from("backdrop").select("*").eq("userId", userId),
+    ]);
 
-    if (error) {
-      console.error("Supabase Error:", error);
-      return NextResponse.json({ error: error.message }, { status: 500 });
+    if (assetError || backdropError) {
+      console.error("Asset Error:", assetError);
+      console.error("Backdrop Error:", backdropError);
+      return NextResponse.json(
+        { error: assetError?.message || backdropError?.message },
+        { status: 500 },
+      );
     }
 
-    return NextResponse.json(data);
+    return NextResponse.json({ backdropData, assetData });
   } catch (err) {
     console.error("API Error:", err);
     return NextResponse.json(
