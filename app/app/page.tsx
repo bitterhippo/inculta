02b1917f80de41a8604fd679db2424295302e5b8
neror <bitterhippo@gmail.com>
2026-04-04 08:36:29 -0700

    fix(app/page): fixes the arrow function notation for instantiating the LoggedInUserPage which prevented next from detecting it

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,3 +1,17 @@
-export const LoggedInUserPage = () => {
-  return <>LOL!</>;
-};
+"use client";
+
+import { SideBar, ExpandableContainer, LongButton } from "@/components";
+
+export default function LoggedInUserPage() {
+  return (
+    <>
+      <SideBar>
+        <LongButton
+          label="Create New Campaign"
+          onClick={() => console.log("Campaign Creation event triggerd")}
+        />
+        <ExpandableContainer categoryName="Available Campaigns"></ExpandableContainer>
+      </SideBar>
+    </>
+  );
+}
