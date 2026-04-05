98693f2ad67deb6b1db40d6cdde2815aefeb0042
neror <bitterhippo@gmail.com>
2026-04-04 23:19:24 -0700

    feat(campaign-creation/page): adds dialog and associated state

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,17 +1,31 @@
 "use client";
 
-import { SideBar, ExpandableContainer, LongButton } from "@/components";
+import { SideBar, ExpandableContainer, LongButton, Dialog } from "@/components";
+import { useState, useEffect } from "react";
+import { createPortal } from "react-dom";
 
 export default function LoggedInUserPage() {
+  const [campaignCreationDialog, campaignCreationDialogToggle] =
+    useState<boolean>(false);
+
+  const modalToggleHandler = () => {
+    campaignCreationDialogToggle((previousValue: boolean) => {
+      return !previousValue;
+    });
+  };
+
+  useEffect(() => {}, [campaignCreationDialog]);
+
   return (
     <>
       <SideBar>
         <LongButton
           label="Create New Campaign"
-          onClick={() => console.log("Campaign Creation event triggerd")}
+          onClick={() => modalToggleHandler()}
         />
         <ExpandableContainer categoryName="Available Campaigns"></ExpandableContainer>
       </SideBar>
+      {campaignCreationDialog && createPortal(<Dialog />, document.body)}
     </>
   );
 }
