6674d6df667b236d24742951bde12b1cedf96c74
neror <bitterhippo@gmail.com>
2026-04-06 18:56:10 -0700

    styles(campaignCreationDialog): updates styling of component to match mocks - eliminates some redundant styling

--- features/campaignCreation/components/CampaignCreationDialog/campaignCreationDialog.tsx
+++ features/campaignCreation/components/CampaignCreationDialog/campaignCreationDialog.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Dialog, LongButton } from "@/components";
+import { Dialog, LongButton, SelectableImageContainer } from "@/components";
 import type { campaignCreationDialogProps } from "./types";
 import styles from "./styles.module.css";
 
@@ -7,6 +7,8 @@ export const CampaignCreationDialog = ({
   onClose,
 }: campaignCreationDialogProps) => {
   const [campaignNameText, setCampaignNameText] = useState<string>("");
+  const [selectedCampaignType, setSelectedCampaignType] =
+    useState<string>("sm");
 
   return (
     <Dialog onClose={onClose}>
@@ -18,26 +20,50 @@ export const CampaignCreationDialog = ({
         </div>
         {/* TODO: Inputs */}
         <div className={styles.CampaignCreationInputContainer}>
-          <p className={styles.CampaignCreationInputLabel}>
+          <label className={styles.CampaignCreationInputLabel}>
             CRUSADE DESIGNATION
-          </p>
+          </label>
           <input
             className={styles.CampaignCreationInput}
             onChange={(e) => setCampaignNameText(e.target.value)}
-            placeholder={
-              campaignNameText.length < 1
-                ? "ENTER CAMPAIGN NAME..."
-                : campaignNameText
-            }
-          ></input>
+            placeholder={"ENTER CAMPAIGN NAME..."}
+            value={campaignNameText}
+          />
         </div>
-        <div className={styles.CampaignCreationSizeSelectorContainer}></div>
+        <div className={styles.CampaignCreationSizeSelectorWrapper}>
+          <label className={styles.CampaignCreationInputLabel}>
+            CANVAS SIZE - SMALL TO LARGE
+          </label>
+          <div className={styles.CampaignCreationSizeSelectorContainer}>
+            <SelectableImageContainer
+              onClick={() => setSelectedCampaignType("sm")}
+              isSelected={selectedCampaignType === "sm"}
+              imgProps={{
+                src: `https://res.cloudinary.com/dpwjuknc3/image/upload/v1774665994/jsurva9f65fqmna8e8br.png`,
+              }}
+            />
+            <SelectableImageContainer
+              onClick={() => setSelectedCampaignType("md")}
+              isSelected={selectedCampaignType === "md"}
+              imgProps={{
+                src: `https://res.cloudinary.com/dpwjuknc3/image/upload/v1774665994/jsurva9f65fqmna8e8br.png`,
+              }}
+            />
+            <SelectableImageContainer
+              onClick={() => setSelectedCampaignType("lg")}
+              isSelected={selectedCampaignType === "lg"}
+              imgProps={{
+                src: `https://res.cloudinary.com/dpwjuknc3/image/upload/v1774665994/jsurva9f65fqmna8e8br.png`,
+              }}
+            />
+          </div>
+        </div>
         <div className={styles.CampaignCreationDialogActionButtonRow}>
           <LongButton
             label="Create"
             onClick={() => console.log("create campaign")}
           />
-          <LongButton label="Cancel" onClick={() => console.log("onClose")} />
+          <LongButton label="Cancel" onClick={onClose} />
         </div>
       </div>
     </Dialog>
