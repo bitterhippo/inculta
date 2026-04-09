423aa649ff80a79b594ec66198070104e0f6eafb
neror <bitterhippo@gmail.com>
2026-04-09 15:04:16 -0700

    style(expandableContainer): updates styling for placeholder text

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -7,7 +7,7 @@ import { createPortal } from "react-dom";
 import { CampaignCreationDialog } from "@/features/campaignCreation/components";
 
 export default function LoggedInUserPage() {
-  const [campaignData, setCampaignData] = useState([]);
+  const [campaignData, setCampaignData] = useState();
   const [campaignCreationDialog, campaignCreationDialogToggle] =
     useState<boolean>(false);
 
@@ -17,6 +17,8 @@ export default function LoggedInUserPage() {
     });
   };
 
+  console.log(campaignData);
+
   useEffect(() => {
     async function fetchCampaignData() {
       const response = await fetch("/api/campaigns", {
@@ -24,14 +26,12 @@ export default function LoggedInUserPage() {
         headers: { "Content-Type": "application/json" },
         credentials: "include",
       });
-      const data = await response.json();
+      const { data } = await response.json();
       setCampaignData(data);
     }
 
     fetchCampaignData();
   }, [campaignCreationDialog]);
-
-  console.log(campaignData);
 
   return (
     <>
@@ -40,11 +40,17 @@ export default function LoggedInUserPage() {
           label="Create New Campaign"
           onClick={() => modalToggleHandler()}
         />
-        <ExpandableContainer categoryName="Current Campaigns">
-          {campaignData?.length < 1 ? (
-            <p>Create there are no active campaigns</p>
+        <ExpandableContainer
+          categoryName="Current Campaigns"
+          placeholder="No active campaigns found"
+        >
+          {campaignData.length > 0 ? (
+            campaignData.map(({ campaign_name }) => {
+              console.log(campaign_name);
+              return <p key={`campaign_name`}>{campaign_name}</p>;
+            })
           ) : (
-            <p>you have campaigns</p>
+            <>not lol</>
           )}
         </ExpandableContainer>
       </SideBar>
