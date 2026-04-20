ebddfbd43e7abf344d23f4086b50f2c5ab6061d7
neror <bitterhippo@gmail.com>
2026-04-19 22:31:19 -0700

    feat(CampaignEditorSideBar): adds back navigation button from a specific campaign to logged in campaign section

--- features/campaignEditor/components/CampaignEditorSideBar/CampaignEditorSideBar.tsx
+++ features/campaignEditor/components/CampaignEditorSideBar/CampaignEditorSideBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./styles.module.css";
 
 import {
@@ -22,74 +23,82 @@ export const CampaignEditorSideBar = ({
 }) => {
   const [selectedTab, setSelectedTab] = useState("Assets");
 
+  const router = useRouter();
   const selectedTabDialogAccessor = selectedTab.toLowerCase();
 
   return (
     <SideBar>
-      <SelectTabList
-        tabListData={CampaignEditorSideBarTabListOptions}
-        selectorHandler={setSelectedTab}
-        selectedTab={selectedTab}
-      />
-      <LongButton
-        label={"Save Project"}
-        iconName={"save"}
-        onClick={() => console.log("lol")}
-      />
-      <LongButton
-        label={`Create New ${selectedTab}`}
-        onClick={() =>
-          setDialogOpen(({ open }) => ({
-            open: !open,
-            source: selectedTabDialogAccessor,
-          }))
-        }
-      />
-      {selectedTabDialogAccessor === "assets" && (
-        <ExpandableContainer categoryName="Assets">
-          {userData?.assetData &&
-            userData.assetData.map(({ id, image_url }) => {
-              return (
-                <DraggableWrapper
-                  id={`${id}-${image_url}`}
-                  key={`${id}-${image_url}`}
-                  inToolbar={true}
-                  image_url={`${image_url}`}
-                >
-                  <img
-                    style={{ maxHeight: "32px", maxWidth: "32px" }}
-                    src={image_url}
-                    alt={id}
-                  />
-                </DraggableWrapper>
-              );
-            })}
-        </ExpandableContainer>
-      )}
-      {selectedTabDialogAccessor === "layers" && (
-        <ExpandableContainer categoryName="Layers">
-          <div className={styles.twoColumnLayout}>
-            {/*TODO: Add default colors here */}
-            {userData?.backdropData &&
-              userData.backdropData.map(
-                ({ image_url, id }: { image_url: string; id: string }) => {
-                  return (
-                    <SelectableImageContainer
-                      key={id}
-                      onClick={() =>
-                        setSelectedBackground({ backgroundImage: image_url })
-                      }
-                      isSelected={
-                        image_url === selectedBackground?.backgroundImage
-                      }
-                      imgProps={{ src: `${image_url}` }}
+      <div className={styles.CampaignEditorSideBarContentsWrapper}>
+        <LongButton
+          label={"Previous Screen"}
+          iconName={"arrowLeftCircle"}
+          onClick={() => router.back()}
+        />
+        <SelectTabList
+          tabListData={CampaignEditorSideBarTabListOptions}
+          selectorHandler={setSelectedTab}
+          selectedTab={selectedTab}
+        />
+        <LongButton
+          label={"Save Project"}
+          iconName={"save"}
+          onClick={() => console.log("lol")}
+        />
+        <LongButton
+          label={`Create New ${selectedTab}`}
+          onClick={() =>
+            setDialogOpen(({ open }) => ({
+              open: !open,
+              source: selectedTabDialogAccessor,
+            }))
+          }
+        />
+        {selectedTabDialogAccessor === "assets" && (
+          <ExpandableContainer categoryName="Assets">
+            {userData?.assetData &&
+              userData.assetData.map(({ id, image_url }) => {
+                return (
+                  <DraggableWrapper
+                    id={`${id}-${image_url}`}
+                    key={`${id}-${image_url}`}
+                    inToolbar={true}
+                    image_url={`${image_url}`}
+                  >
+                    <img
+                      style={{ maxHeight: "32px", maxWidth: "32px" }}
+                      src={image_url}
+                      alt={id}
                     />
-                  );
-                },
-              )}
-          </div>
-        </ExpandableContainer>
-      )}
+                  </DraggableWrapper>
+                );
+              })}
+          </ExpandableContainer>
+        )}
+        {selectedTabDialogAccessor === "layers" && (
+          <ExpandableContainer categoryName="Layers">
+            <div className={styles.twoColumnLayout}>
+              {/*TODO: Add default colors here */}
+              {userData?.backdropData &&
+                userData.backdropData.map(
+                  ({ image_url, id }: { image_url: string; id: string }) => {
+                    return (
+                      <SelectableImageContainer
+                        key={id}
+                        onClick={() =>
+                          setSelectedBackground({ backgroundImage: image_url })
+                        }
+                        isSelected={
+                          image_url === selectedBackground?.backgroundImage
+                        }
+                        imgProps={{ src: `${image_url}` }}
+                      />
+                    );
+                  },
+                )}
+            </div>
+          </ExpandableContainer>
+        )}
+      </div>
     </SideBar>
   );
 };
